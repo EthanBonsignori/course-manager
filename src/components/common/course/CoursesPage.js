e6ef8f1dddfa19848aa9a4b33ad3a9e1cc513abe
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-24 15:08:40 -0400

    setup redux

--- src/components/common/course/CoursesPage.js
+++ src/components/common/course/CoursesPage.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CoursesPage = () => (<h2>Courses</h2>);
+const CoursesPage = () => {
+  const [formState, setFormState] = useState({
+    title: '',
+  });
+
+  const handleChange = (event) => {
+    const course = { ...formState, title: event.target.value };
+    setFormState(course);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input type='text' onChange={handleChange} value={formState.title} />
+      <input type='submit' value='Save' />
+    </form>
+  );
+};
 
 export default CoursesPage;
