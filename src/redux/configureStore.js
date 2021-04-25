f1d443bc2f130aa527da9e35be156082d87bb250
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-25 15:31:10 -0400

    fix devtools bug

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,7 +5,7 @@ import rootReducer from './reducers';
 
 export default function configureStore(initialState) {
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   return createStore(
     rootReducer,
