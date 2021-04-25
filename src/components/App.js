22d06f80b5e4aea70241eb75d0f39144933a2b67
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-25 15:30:20 -0400

    add toasts

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import AboutPage from './AboutPage';
-import CoursesPage from './course/CoursesPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AboutPage from './about/AboutPage';
 import Header from './common/Header';
-import HomePage from './HomePage';
+import CoursesPage from './courses/CoursesPage';
+import ManageCoursePage from './courses/ManageCoursePage';
+import HomePage from './home/HomePage';
 import PageNotFound from './PageNotFound';
-import ManageCoursePage from './course/ManageCoursePage';
 
 const App = () => (
   <div className='container-fluid'>
@@ -18,6 +20,7 @@ const App = () => (
       <Route path='/course' component={ManageCoursePage} />
       <Route component={PageNotFound} />
     </Switch>
+    <ToastContainer autoClose={3000} hideProgressBar />
   </div>
 );
 
