7b9b3c64aac2284f430eb633270fcb0398218461
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-25 12:24:23 -0400

    add success action

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,7 +1,21 @@
 import * as types from './actionTypes';
+import * as courseApi from '../../api/courseApi';
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
 }
 
-export const placeholder = '';
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_AUTHORS_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return async (dispatch) => {
+    try {
+      const courses = await courseApi.getCourses();
+      return dispatch(loadCourseSuccess(courses));
+    } catch (error) {
+      return console.error(error);
+    }
+  };
+}
