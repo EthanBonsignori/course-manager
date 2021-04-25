71fd40b3157c6550c4a566b3bce88b5ec507cda4
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-25 14:02:35 -0400

    add save and update course

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,12 +1,16 @@
 import * as types from './actionTypes';
 import * as courseApi from '../../api/courseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export function loadCourseSuccess(courses) {
-  return { type: types.LOAD_AUTHORS_SUCCESS, courses };
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -14,6 +18,19 @@ export function loadCourses() {
     try {
       const courses = await courseApi.getCourses();
       return dispatch(loadCourseSuccess(courses));
+    } catch (error) {
+      return console.error(error);
+    }
+  };
+}
+
+export function saveCourse(course) {
+  return async (dispatch) => {
+    try {
+      const savedCourse = await courseApi.saveCourse(course);
+      return course.id
+        ? dispatch(updateCourseSuccess(savedCourse))
+        : dispatch(createCourseSuccess(savedCourse));
     } catch (error) {
       return console.error(error);
     }
